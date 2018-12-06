569dc12d02ccb87ef83bab3e93c87129aaaec285
ljm <yhxljm@qq.com>
2018-12-06 15:34:15 +0800

    新增内存的相关知识，

---
+++ ListDemo/ListMainForm.h
@@ -0,0 +1,143 @@
+#pragma once
+
+/*
+* Code By Tojen (qq:342269237)
+* 界面设计图片资源80%原创，布局完全原创,学习作品，不好请拍砖
+*/
+#include <objbase.h>
+#include <zmouse.h>
+#include <exdisp.h>
+#include <comdef.h>
+#include <vector>
+#include <sstream>
+
+#include "..\DuiLib\UIlib.h"
+
+using namespace DuiLib;
+
+#include "MenuWnd.h"
+#define WM_ADDLISTITEM WM_USER + 50
+
+/*
+* 存放第二列数据
+*/
+std::vector<std::string> domain;
+
+/*
+* 存放第三列数据
+*/
+std::vector<std::string> desc;
+
+
+/*
+*  线程函数中传入的结构体变量，使用线程为了使界面线程立即返回，防止卡住，你们懂得。
+*/
+struct Prama
+{
+	HWND hWnd;
+	CListUI* pList;
+	CButtonUI* pSearch;
+	CDuiString tDomain;
+};
+
+struct TestMem {
+	int count;
+};
+
+
+class ListMainForm : public CWindowWnd, public INotifyUI, public IListCallbackUI
+{
+public:
+    ListMainForm() {
+    };
+
+    LPCTSTR GetWindowClassName() const 
+    { 
+        return _T("ScanMainForm"); 
+    };
+
+    UINT GetClassStyle() const
+    { 
+        return CS_DBLCLKS; 
+    };
+
+    void OnFinalMessage(HWND /*hWnd*/) 
+    { 
+        delete this;
+    };
+
+	void Init();
+
+    void OnPrepare(TNotifyUI& msg) 
+    {
+
+    }
+
+	static DWORD WINAPI Search(LPVOID lpParameter);
+	
+	void OnSearch();
+ 
+	LPCTSTR GetItemText(CControlUI* pControl, int iIndex, int iSubItem);
+
+	void Notify(TNotifyUI& msg);
+
+	LRESULT OnAddListItem(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+	LRESULT OnCreate(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+    LRESULT OnClose(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        bHandled = FALSE;
+        return 0;
+    }
+
+    LRESULT OnDestroy(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        ::PostQuitMessage(0L);
+        bHandled = FALSE;
+        return 0;
+    }
+
+    LRESULT OnNcActivate(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        if( ::IsIconic(*this) ) bHandled = FALSE;
+        return (wParam == 0) ? TRUE : FALSE;
+    }
+
+    LRESULT OnNcCalcSize(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        return 0;
+    }
+
+    LRESULT OnNcPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        return 0;
+    }
+
+	LRESULT OnNcHitTest(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+	/**
+	*大小改变的回调函数，可以尝试注释，修改代码 看效果
+	*/
+	LRESULT OnSize(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+	LRESULT OnGetMinMaxInfo(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+	LRESULT OnSysCommand(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+	/***
+	* 处理来着windows系统的各种消息，WM= windows  message
+	*/
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+public:
+    CPaintManagerUI m_pm;
+
+private:
+    CButtonUI* m_pCloseBtn;  //此处成员变量用m_ 开头，请务必准守，不对外暴露的成员一律用private修饰
+    CButtonUI* m_pMaxBtn;
+    CButtonUI* m_pRestoreBtn;
+    CButtonUI* m_pMinBtn;
+    CButtonUI* m_pSearch;
+
+};
