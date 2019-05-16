6e1b907cd8448e7b3bcb81ef30b75e3f6a9ffa76
ljm <yhxljm@qq.com>
2019-05-16 11:35:59 +0800

    删除不必要的文件

--- IExplorerDemo/GameDemo.cpp
+++ IExplorerDemo/GameDemo.cpp
@@ -188,96 +188,9 @@ public:
     }
 
     void OnPrepare() { 
-        GameListUI* pGameList = static_cast<GameListUI*>(m_pm.FindControl(_T("gamelist")));
-        GameListUI::Node* pCategoryNode = NULL;
-        GameListUI::Node* pGameNode = NULL;
-        GameListUI::Node* pServerNode = NULL;
-        GameListUI::Node* pRoomNode = NULL;
-        pCategoryNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 3}{x 4}推荐游戏"));
-        for( int i = 0; i < 4; ++i )
-        {
-            pGameNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 10}{x 4}四人斗地主"), pCategoryNode);
-            for( int i = 0; i < 3; ++i )
-            {
-                pServerNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 10}{x 4}测试服务器"), pGameNode);
-                for( int i = 0; i < 3; ++i )
-                {
-                    pRoomNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 10}{x 4}测试房间"), pServerNode);
-                }
-            }
-        }
-        pCategoryNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 3}{x 4}最近玩过的游戏"));
-        for( int i = 0; i < 2; ++i )
-        {
-            pGameList->AddNode(_T("三缺一"), pCategoryNode);
-        }
-        pCategoryNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 3}{x 4}棋牌游戏"));
-        for( int i = 0; i < 8; ++i )
-        {
-            pGameList->AddNode(_T("双扣"), pCategoryNode);
-        }
-        pCategoryNode = pGameList->AddNode(_T("{x 4}{i gameicons.png 18 3}{x 4}休闲游戏"));
-        for( int i = 0; i < 8; ++i )
-        {
-            pGameList->AddNode(_T("飞行棋"), pCategoryNode);
-        }
 
-        CListUI* pUserList = static_cast<CListUI*>(m_pm.FindControl(_T("userlist")));
-        pUserList->SetTextCallback(this);      
-        for( int i = 0; i < 400; i++ ) {
-            CListTextElementUI* pListElement = new CListTextElementUI;
-            pUserList->Add(pListElement);
-        }
-
-        CLoginFrameWnd* pLoginFrame = new CLoginFrameWnd();
-        if( pLoginFrame == NULL ) { Close(); return; }
-        pLoginFrame->Create(m_hWnd, _T(""), UI_WNDSTYLE_DIALOG, 0, 0, 0, 0, 0, NULL);
-		pLoginFrame->SetIcon(IDI_ICON_DUILIB);
-        pLoginFrame->CenterWindow();
-        pLoginFrame->ShowModal();
     }
 
-    void SendChatMessage() {
-        CEditUI* pChatEdit = static_cast<CEditUI*>(m_pm.FindControl(_T("chatEdit")));
-        if( pChatEdit == NULL ) return;
-        pChatEdit->SetFocus();
-        if( pChatEdit->GetText().IsEmpty() ) return;
-
-        CRichEditUI* pRichEdit = static_cast<CRichEditUI*>(m_pm.FindControl(_T("chatmsglist")));
-        if( pRichEdit == NULL ) return;
-        long lSelBegin = 0, lSelEnd = 0;
-        CHARFORMAT2 cf;
-        ZeroMemory(&cf, sizeof(CHARFORMAT2));
-        cf.cbSize = sizeof(cf);
-        cf.dwReserved = 0;
-        cf.dwMask = CFM_COLOR;
-        cf.crTextColor = RGB(220, 0, 0);
-
-        lSelEnd = lSelBegin = pRichEdit->GetTextLength();
-        pRichEdit->SetSel(lSelEnd, lSelEnd);
-        pRichEdit->ReplaceSel(_T("某人"), false);
-        lSelEnd = pRichEdit->GetTextLength();
-        pRichEdit->SetSel(lSelBegin, lSelEnd);
-        pRichEdit->SetSelectionCharFormat(cf);
-
-        lSelBegin = lSelEnd;
-        pRichEdit->SetSel(-1, -1);
-        pRichEdit->ReplaceSel(_T("说:"), false);
-
-        pRichEdit->SetSel(-1, -1);
-        pRichEdit->ReplaceSel(pChatEdit->GetText(), false);
-        pChatEdit->SetText(_T(""));
-
-        pRichEdit->SetSel(-1, -1);
-        pRichEdit->ReplaceSel(_T("\n"), false);
-
-        cf.crTextColor = RGB(0, 0, 0);
-        lSelEnd = pRichEdit->GetTextLength();
-        pRichEdit->SetSel(lSelBegin, lSelEnd);
-        pRichEdit->SetSelectionCharFormat(cf);
-
-        pRichEdit->EndDown();
-    }
 
     void Notify(TNotifyUI& msg)
     {
@@ -301,132 +214,12 @@ public:
             if( name == _T("quitbtn") ) {
                 /*Close()*/PostQuitMessage(0); // 因为activex的原因，使用close可能会出现错误
             }
-            else if( name == _T("returnhallbtn") ) {
-                CControlUI* pFadeControl = m_pm.FindControl(_T("fadeEffect"));
-                if( pFadeControl ) pFadeControl->SetVisible(false); 
 
-                COptionUI* pControl = static_cast<COptionUI*>(m_pm.FindControl(_T("hallswitch")));
-                pControl->Activate();
-                pControl = static_cast<COptionUI*>(m_pm.FindControl(_T("roomswitch")));
-                if( pControl ) pControl->SetVisible(false);
-            }
-            else if( name == _T("fontswitch") ) {
-                TFontInfo* pFontInfo = m_pm.GetDefaultFontInfo();
-                if( pFontInfo->iSize < 18 ) {
-                    TFontInfo* pFontInfo = m_pm.GetFontInfo(0);
-                    if( pFontInfo )m_pm.SetDefaultFont(pFontInfo->sFontName, pFontInfo->iSize, pFontInfo->bBold, 
-                        pFontInfo->bUnderline, pFontInfo->bItalic);
-                }
-                else {
-                    TFontInfo* pFontInfo = m_pm.GetFontInfo(1);
-                    if( pFontInfo )m_pm.SetDefaultFont(pFontInfo->sFontName, pFontInfo->iSize, pFontInfo->bBold, 
-                        pFontInfo->bUnderline, pFontInfo->bItalic);
-                }
-                m_pm.GetRoot()->NeedUpdate();
-            }
-            else if( name == _T("leaveBtn")  || name == _T("roomclosebtn") ) {
-                COptionUI* pControl = static_cast<COptionUI*>(m_pm.FindControl(_T("hallswitch")));
-                if( pControl ) {
-                    pControl->Activate();
-                    pControl = static_cast<COptionUI*>(m_pm.FindControl(_T("roomswitch")));
-                    if( pControl ) pControl->SetVisible(false);
-                }
-            }
-            else if( name == _T("sendbtn") ) {
-                SendChatMessage();
-            }
-        }
-        else if( msg.sType == _T("selectchanged") ) {
-            CDuiString name = msg.pSender->GetName();
-            if( name == _T("hallswitch") ) {
-                CTabLayoutUI* pControl = static_cast<CTabLayoutUI*>(m_pm.FindControl(_T("switch")));
-                if( pControl && pControl->GetCurSel() != 0 ) pControl->SelectItem(0);
-            }
-            else if( name == _T("roomswitch") ) {
-                CTabLayoutUI* pControl = static_cast<CTabLayoutUI*>(m_pm.FindControl(_T("switch")));
-                if( pControl && pControl->GetCurSel() != 1 ) {
-                    pControl->SelectItem(1);
-                    DeskListUI* pDeskList = static_cast<DeskListUI*>(m_pm.FindControl(_T("destlist")));
-                    pDeskList->SetFocus();
-                    CRichEditUI* pRichEdit = static_cast<CRichEditUI*>(m_pm.FindControl(_T("chatmsglist")));
-                    if( pRichEdit ) {
-                        pRichEdit->SetText(_T("欢迎进入XXX游戏，祝游戏愉快！\n\n"));
-                        long lSelBegin = 0, lSelEnd = 0;
-                        CHARFORMAT2 cf;
-                        ZeroMemory(&cf, sizeof(CHARFORMAT2));
-                        cf.cbSize = sizeof(cf);
-                        cf.dwReserved = 0;
-                        cf.dwMask = CFM_COLOR;
-                        cf.crTextColor = RGB(255, 0, 0);
-                        lSelEnd = pRichEdit->GetTextLength();
-                        pRichEdit->SetSel(lSelBegin, lSelEnd);
-                        pRichEdit->SetSelectionCharFormat(cf);
-                    }
-                }
-            }
-        }
-        else if( msg.sType == _T("itemclick") ) {
-            GameListUI* pGameList = static_cast<GameListUI*>(m_pm.FindControl(_T("gamelist")));
-            if( pGameList->GetItemIndex(msg.pSender) != -1 )
-            {
-                if( _tcscmp(msg.pSender->GetClass(), DUI_CTR_LISTLABELELEMENT) == 0 ) {
-                    GameListUI::Node* node = (GameListUI::Node*)msg.pSender->GetTag();
-
-                    POINT pt = { 0 };
-                    ::GetCursorPos(&pt);
-                    ::ScreenToClient(m_pm.GetPaintWindow(), &pt);
-                    pt.x -= msg.pSender->GetX();
-                    SIZE sz = pGameList->GetExpanderSizeX(node);
-                    if( pt.x >= sz.cx && pt.x < sz.cy )                     
-                        pGameList->ExpandNode(node, !node->data()._expand);
-                }
-            }
-        }
-        else if( msg.sType == _T("itemactivate") ) {
-            GameListUI* pGameList = static_cast<GameListUI*>(m_pm.FindControl(_T("gamelist")));
-            if( pGameList->GetItemIndex(msg.pSender) != -1 )
-            {
-                if( _tcscmp(msg.pSender->GetClass(), DUI_CTR_LISTLABELELEMENT) == 0 ) {
-                    GameListUI::Node* node = (GameListUI::Node*)msg.pSender->GetTag();
-                    pGameList->ExpandNode(node, !node->data()._expand);
-                    if( node->data()._level == 3 ) {
-                        COptionUI* pControl = static_cast<COptionUI*>(m_pm.FindControl(_T("roomswitch")));
-                        if( pControl ) {
-                            pControl->SetVisible(true);
-                            pControl->SetText(node->parent()->parent()->data()._text);
-                            pControl->Activate();
-
-                        }
-                    }
-                }
-            }
-        }
-        else if( msg.sType == _T("itemselect") ) {
-            if( msg.pSender->GetName() == _T("chatCombo") ) {
-                CEditUI* pChatEdit = static_cast<CEditUI*>(m_pm.FindControl(_T("chatEdit")));
-                if( pChatEdit ) pChatEdit->SetText(msg.pSender->GetText());
-                static_cast<CComboUI*>(msg.pSender)->SelectItem(-1);
-            }
-        }
-        else if( msg.sType == _T("return") ) {
-            if( msg.pSender->GetName() == _T("chatEdit") ) {
-                SendChatMessage();
-            }
         }
     }
 
     LPCTSTR GetItemText(CControlUI* pControl, int iIndex, int iSubItem)
     {
-        if( pControl->GetParent()->GetParent()->GetName() == _T("userlist") ) {
-            if( iSubItem == 0 ) return _T("<i vip.png>");
-            if( iSubItem == 1 ) return _T("<i vip.png>");
-            if( iSubItem == 2 ) return _T("此人昵称");
-            if( iSubItem == 3 ) return _T("5");
-            if( iSubItem == 4 ) return _T("50%");
-            if( iSubItem == 5 ) return _T("0%");
-            if( iSubItem == 6 ) return _T("100");
-        }
-
         return _T("");
     }
 
@@ -439,7 +232,7 @@ public:
         m_pm.Init(m_hWnd);
         CDialogBuilder builder;
         CDialogBuilderCallbackEx cb;
-        CControlUI* pRoot = builder.Create(_T("hall.xml"), (UINT)0, &cb, &m_pm);
+        CControlUI* pRoot = builder.Create(_T("iexplorer.xml"), (UINT)0, &cb, &m_pm);
         ASSERT(pRoot && "Failed to parse XML");
         m_pm.AttachDialog(pRoot);
         m_pm.AddNotifier(this);
